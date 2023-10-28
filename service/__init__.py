9a4831baf14dd04d086920a22abdc8e13bc5612d
Andrey Pomortsev <andreypomortsev@gmail.com>
2023-10-28 07:29:16 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -13,7 +13,17 @@ from flask_talisman import Talisman
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
-talisman = Talisman(app)
+
+# Included this for "X-XSS-Protection"
+csp = {
+    'default-src': "'self'",
+    'object-src': "'none'",
+    'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+}
+talisman = Talisman(
+    app,
+    content_security_policy=csp
+)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -191,10 +191,10 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         headers = {
             "X-Frame-Options": "SAMEORIGIN",
-            "X-XSS-Protection": "1; mode=block",
             "X-Content-Type-Options": "nosniff",
-            "Content-Security-Policy": "default-src 'self'; object-src 'none'",
+            "Content-Security-Policy": "default-src 'self'; object-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'",
             "Referrer-Policy": "strict-origin-when-cross-origin",
+            # "X-XSS-Protection": "1; mode=block", due to its outdated status I changed it to CSP
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
